f8d60fac0d9ba006a3afb1d13f37110352745d03
hualiping <823227806@qq.com>
2018-01-01 21:47:47 +0800

    login

--- js/scripts.js
+++ js/scripts.js
@@ -34,50 +34,110 @@ jQuery(document).ready(function() {
             }
         }
 
+        // localStorage.setItem("userName",username);
+        console.log("sdafadfadsf"+localStorage.getItem("b"));
     });
 
-    $('.page-container form').submit(function(){
-        var username = $(this).find('.username').val();
-        var password = $(this).find('.password').val();
-        var rePassword = $(this).find('.rePassword').val();
-        if(username == '') {
-            $(this).find('.errorback0').fadeIn();
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.username').focus();
-            });
-            return false;
-        }
-        if(password == '') {
-            $(this).parent().find('.errorback1').fadeIn();
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.password').focus();
-            });
-            return false;
-        }
-        if(rePassword == '') {
-            $(this).parent().find('.errorback2').fadeIn();
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.rePassword').focus();
-            });
-            return false;
-        }
-        else if(rePassword.length<6||rePassword.length>10){
-            $(this).find('.errorback1').fadeIn();
-            $(this).find('.errorText_password').fadeIn();
-            console.log(rePassword.length);
-            return false;
-        }
 
-        if($(this).find('.password').val()!=$(this).find('.repassword').val()){
-            $(this).find('.errorback2').fadeIn();
-            $(this).find('.errorText_repassword').fadeIn();
-            return false;
-        }
-    });
+    // 登录确定按钮事件，提交表单
+    // $('.page-container form').submit(function(){
+    //     console.log("login");
+    //     var  username="hlp";
+    //     // var username = $(this).find('.username').val();
+    //     // var password = $(this).find('.password').val();
+    //     // var rePassword = $(this).find('.rePassword').val();
+    //     // if(username == '') {
+    //     //     $(this).find('.errorback0').fadeIn();
+    //     //     $(this).find('.error').fadeIn('fast', function(){
+    //     //         $(this).parent().find('.username').focus();
+    //     //     });
+    //     //     return false;
+    //     // }
+    //     // if(password == '') {
+    //     //     $(this).parent().find('.errorback1').fadeIn();
+    //     //     $(this).find('.error').fadeIn('fast', function(){
+    //     //         $(this).parent().find('.password').focus();
+    //     //     });
+    //     //     return false;
+    //     // }
+    //     // if(rePassword == '') {
+    //     //     console.log("rePassword");
+    //     //     $(this).parent().find('.errorback2').fadeIn();
+    //     //     $(this).find('.error').fadeIn('fast', function(){
+    //     //         $(this).parent().find('.rePassword').focus();
+    //     //     });
+    //     //     return false;
+    //     // }
+    //     // else if(rePassword.length<6||rePassword.length>10){
+    //     //     $(this).find('.errorback1').fadeIn();
+    //     //     $(this).find('.errorText_password').fadeIn();
+    //     //     console.log(rePassword.length);
+    //     //     return false;
+    //     // }
+    //     //
+    //     // if($(this).find('.password').val()!=$(this).find('.repassword').val()){
+    //     //     $(this).find('.errorback2').fadeIn();
+    //     //     $(this).find('.errorText_repassword').fadeIn();
+    //     //     return false;
+    //     // }
+    //
+    //
+    //
+    //     // localStorage.setItem("userName",username);
+    //     // window.location.href= "test.html";
+    //     alert("登录成功！");
+    //     window.location.href = "https://www.baidu.com";
+    //     window.location.href= "test.html";
+    //     // return true;
+    //     // self.location = "test.html";
+    //     // console.log("sdafadfadsf"+localStorage.getItem("b"));
+    // });
+
+
 
     $('.page-container form .username, .page-container form .password').keyup(function(){
         $(this).parent().find('.error').fadeOut('fast');
         $(this).parent().find('.errorText').fadeOut('fast');
     });
 
+    jQuery('#button').click(function () {
+        var username="aaa";
+        localStorage.setItem("userName",username);
+        window.location.href= "gameRoom.html";
+        console.log("adfsd");
+        alert("登录成功！");
+        
+    });
+    // $('#button').onclick(function(){
+    //     var username = $(this).parent().find('.username').val();
+    //     var password = $(this).parent().find('.password').val();
+    //     console.log(username+password);
+    //     console.log($(this).innerText);
+    //     if (username == "a" && password == "123") {
+    //         alert("登录成功！");
+    //         localStorage.setItem("userName",username);
+    //         location.href = "test.html";
+    //     } else {
+    //         alert("登录失败！");
+    //         // myform.onsubmit = new Function("return false");
+    //     }
+    //
+    // });
+    // function login() {
+    //     var username = $(this).parent().find('.username').val();
+    //     var password = $(this).parent().find('.password').val();
+    //     console.log(username+password);
+    //     console.log($(this).innerText);
+    //     if (username == "a" && password == "123") {
+    //         alert("登录成功！");
+    //         localStorage.setItem("userName",username);
+    //         location.href = "test.html";
+    //     } else {
+    //         alert("登录失败！");
+    //         // myform.onsubmit = new Function("return false");
+    //     }
+    // }
+
 });
+
+
